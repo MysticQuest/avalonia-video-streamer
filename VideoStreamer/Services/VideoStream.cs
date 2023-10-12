11f11e134b9df3307fb69668a0ac8c035d97e5e6
Mystic Quest <kirimns@gmail.com>
2023-10-13 02:51:42 +0300

    updates pipeline service

--- VideoStreamer/Services/VideoStream.cs
+++ VideoStreamer/Services/VideoStream.cs
@@ -14,5 +14,31 @@ public class VideoStream
         _pipe = new Pipe();
     }
 
+    public async Task StartStreamingAsync(string url)
+    {
+        var response = await _httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+        response.EnsureSuccessStatusCode();
+
+        var stream = await response.Content.ReadAsStreamAsync();
+
+        var writer = _pipe.Writer;
+        var buffer = writer.GetMemory(4096);
+
+        int read;
+        while ((read = await stream.ReadAsync(buffer)) != 0)
+        {
+            writer.Advance(read);
+            var result = await writer.FlushAsync();
+
+            if (result.IsCompleted)
+            {
+                break;
+            }
+            buffer = writer.GetMemory(4096);
+        }
+
+        writer.Complete();
+    }
+
     public PipeReader GetReader() => _pipe.Reader;
 }
