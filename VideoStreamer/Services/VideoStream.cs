7187bb298f2d042448a21a8f1677d1aa28d41f82
Mystic Quest <kirimns@gmail.com>
2023-10-13 02:48:17 +0300

    adds vs pipeline service

---
+++ VideoStreamer/Services/VideoStream.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO.Pipelines;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class VideoStream
+{
+    private readonly HttpClient _httpClient;
+    private readonly Pipe _pipe;
+
+    public VideoStream()
+    {
+        _httpClient = new HttpClient();
+        _pipe = new Pipe();
+    }
+
+    public PipeReader GetReader() => _pipe.Reader;
+}
