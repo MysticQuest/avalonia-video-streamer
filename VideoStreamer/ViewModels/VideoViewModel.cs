a7bb5cb0ff887ff5add6690d55aaf6a091158792
Mystic Quest <kirimns@gmail.com>
2023-10-13 02:55:18 +0300

    adds view model class

---
+++ VideoStreamer/ViewModels/VideoViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.IO.Pipelines;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoStreamer.ViewModels
+{
+    internal class VideoViewModel : ReactiveObject
+    {
+        private readonly VideoStream _videoStream;
+
+        public ReactiveCommand<Unit, Unit> StartStreamingCommand { get; }
+
+        public VideoViewModel()
+        {
+            _videoStream = new VideoStream();
+            StartStreamingCommand = ReactiveCommand.CreateFromTask(StartStreamingAsync);
+        }
+
+        private async Task StartStreamingAsync()
+        {
+            await _videoStream.StartStreamingAsync("http://example.com/videostream");
+        }
+
+        public PipeReader GetVideoStreamReader() => _videoStream.GetReader();
+    }
+}
